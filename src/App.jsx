1de0ff76a78c345322829db9fe21db36b7250abe
AbroShahzeb <khizr772@gmail.com>
2023-09-27 09:42:25 +0500

    Mobile navigation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Dollat</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
